a623c0b799083025bfc5ca9eaa9184e7f196a66d
Lucas_Toscan <lucptoscan@gmail.com>
2023-05-03 02:15:56 -0300

    exercicios do dia pt2

--- 01-fundamentos/secao-03-introducao-a-javascript/dia3.1/exercios-da-secao/script7.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia3.1/exercios-da-secao/script7.js
@@ -6,3 +6,20 @@ Porcentagem >= 60 -> D
 Porcentagem >= 50 -> E
 Porcentagem < 50 -> F
 O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
+let nota = 58;
+
+if (nota < 0 || nota > 100) {
+  console.log("Error!");
+} else if (nota >= 90) {
+  console.log("A");
+} else if (nota >= 80) {
+  console.log("B");
+} else if (nota >= 70) {
+  console.log("C");
+} else if (nota >= 60) {
+  console.log("D");
+} else if (nota >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
--- 01-fundamentos/secao-03-introducao-a-javascript/dia3.1/exercios-da-secao/script9.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia3.1/exercios-da-secao/script9.js
@@ -1,1 +1,10 @@
 /* 9 - Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false. */
+let n1 = 10;
+let n2 = 20;
+let n3 = 30;
+
+let impar = false;
+if (n1 % 2 !== 0 || n2 % 2 !== 0 || n3 % 2 !== 0) {
+  par = true;
+}
+console.log(impar);
